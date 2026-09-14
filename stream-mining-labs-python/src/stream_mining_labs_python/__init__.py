cd0319b399aec00762294b574e414c657b4206e8
Wai <waiwai00021@gmail.com>
2026-09-14 09:10:01 +0000

    Set up stream-mining-labs-python project structure

---
+++ stream-mining-labs-python/src/stream_mining_labs_python/__init__.py
@@ -0,0 +1,2 @@
+def main() -> None:
+    print("Hello from stream-mining-labs-python!")
---
+++ stream-mining-labs-python/src/stream_mining_labs_python/spendreport/fraud_detection_job.py
@@ -0,0 +1,5 @@
+def main():
+    print("Fraud detection job running")
+
+if __name__ == "__main__":
+    main()
